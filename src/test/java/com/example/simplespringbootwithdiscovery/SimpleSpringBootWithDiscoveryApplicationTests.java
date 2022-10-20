fe6d6f321017cb327be7260f088ae18c6ef7714d
Jonathan Cuningham <jbcuningham@gmail.com>
2022-10-20 09:31:18 -0500

    Working, but only with Java 8

---
+++ src/test/java/com/example/simplespringbootwithdiscovery/SimpleSpringBootWithDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootwithdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootWithDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
